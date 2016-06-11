a13f262d5f5941bd377a1524272cd5cd3bd68930
ZuluPro <montheanthony@hotmail.com>
2016-06-11 00:30:00 -0400

    Added audio model

--- solidaridade/core/admin.py
+++ solidaridade/core/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from core.models import Image
+from core.models import Image, Audio
 
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'preview')
 
+
+class AudioAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'listen')
+
 admin.site.register(Image, ImageAdmin)
+admin.site.register(Audio, AudioAdmin)
--- solidaridade/core/models.py
+++ solidaridade/core/models.py
@@ -14,3 +14,21 @@ class Image(models.Model):
         return '<img src="%s" height="100px"/>' % self.file.url
     preview.short_description = 'Image'
     preview.allow_tags = True
+
+
+class Audio(models.Model):
+    file = models.FileField()
+
+    html_template = '<audio controls><source src=%s type="audio/ogg">\
+          Your browser does not support the audio tag. </audio>'
+
+    class Meta:
+        app_label = 'core'
+
+    def __str__(self):
+        return self.file.url
+
+    def listen(self):
+        return self.html_template % self.file.url
+    listen.short_description = 'Audio'
+    listen.allow_tags = True
